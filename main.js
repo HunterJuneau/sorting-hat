860c38bb9eefd52c5f4e48a85a2ba580cbc434be
HunterJuneau <huntlee17@gmail.com>
2020-07-29 21:29:07 -0500

    another commit

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const openForm = () => {
                                                  <form class="card-body">
                                                  <h1>Enter First Year's Name</h1>
                                                  <label for="name">Student:</label>
-                                                 <input type="text" id="name">
+                                                 <input type="text" id="name" required>
                                                  <button type="button" class="btn btn-secondary" id="sort">Sort!</button>
                                                  </form>
                                                  </div>`;
@@ -20,34 +20,55 @@ const openForm = () => {
 };
 
 const sortListener = () => {
-    document.getElementById('sort').addEventListener('click', getName);
-    document.getElementById('sort').addEventListener('click', cardBuilder);
+	document.getElementById('sort').addEventListener('click', getName);
+	document.getElementById('sort').addEventListener('click', cardBuilder);
 };
 
 const startSorting = () => {
 	document.getElementById('start-sorting').addEventListener('click', openForm);
 };
 
+const getHouse = () => {
+	return Math.floor(Math.random() * 4);
+};
+
 const getName = () => {
-	const name = document.getElementById('name').value;
-	return studentNames.push(name);
+	const studentName = document.getElementById('name').value;
+	studentNames.push({ name: studentName, house: houses[getHouse()] });
 };
 
 const cardBuilder = () => {
 	let domString = '';
 
 	for (let i = 0; i < studentNames.length; i++) {
-        let housePicker = Math.floor(Math.random() * 4)
 		domString += `<div class="card" style="width: 18rem;">
                         <div class="card-body">
-                        <h5 class="card-title">${studentNames[i]}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${houses[housePicker]}</h6>
+                        <h5 class="card-title">${studentNames[i].name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${studentNames[i].house}</h6>
+                        <button type="button" class="btn btn-danger" id="${i}">Expel Student</button>
                         </div>
                     </div>`;
 	}
 	console.log('Hi!');
 
 	printToDom('cards-container', domString);
+	expelListener();
+};
+
+const expelListener = () => {
+	document
+		.getElementById('cards-container')
+		.addEventListener('click', expelStudent);
+};
+
+const expelStudent = (e) => {
+    console.log(e)
+	const ctype = e.target.type;
+	const target = e.target.id;
+	if (ctype === 'button') {
+		studentNames.splice(target, 1);
+		cardBuilder();
+	}
 };
 
 const init = () => {
